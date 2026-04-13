6aa1473e0e4c5a7d6b736334693d2228f253df2b
mustapha <mustaphaamoutaki@gmail.com>
2026-04-13 16:52:30 +0100

    initial commmit

---
+++ src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
@@ -0,0 +1,16 @@
+package com.alexsysSolutions.alexsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AlexsisApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AlexsisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexsysSolutions/alexsis/AlexsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsysSolutions.alexsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
