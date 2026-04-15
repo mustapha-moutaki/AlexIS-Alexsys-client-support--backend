058c20622d63f0b92829963841cabbe2f9eed095
mustapha <mustaphaamoutaki@gmail.com>
2026-04-15 12:23:45 +0100

    feat: create ticket class

---
+++ src/main/java/com/alexsysSolutions/alexsis/model/Ticket.java
@@ -0,0 +1,89 @@
+package com.alexsysSolutions.alexsis.model;
+
+import com.alexsysSolutions.alexsis.enums.IssueType;
+import com.alexsysSolutions.alexsis.enums.Priority;
+import com.alexsysSolutions.alexsis.enums.TicketStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@SequenceGenerator(
+        name = "ticket_seq",
+        sequenceName = "ticket_seq",
+        allocationSize = 50
+)
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ticket_seq")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ticket_status")
+    private TicketStatus ticketStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "priority", nullable = false)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "issue_type", nullable = false)
+    private IssueType issueType;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
+    private java.util.List<Attachment> attachments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_to")
+    private Agent assignedTo;
+
+    private LocalDateTime assignedAt;
+    private LocalDateTime resolvedAt;
+    private LocalDateTime closedAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
