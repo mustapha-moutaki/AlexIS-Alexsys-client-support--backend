ad85e130a5330c7292017e301928fe56baf34caa
mustapha <mustaphaamoutaki@gmail.com>
2026-04-15 09:30:00 +0100

    feat: create util package to simplify hash and verifying the code

---
+++ src/main/java/com/alexsysSolutions/alexsis/util/PasswordUtil.java
@@ -0,0 +1,12 @@
+//package com.alexsysSolutions.alexsis.util;
+//
+//public class PasswordUtil {
+//
+//    public static String hash(String password){
+//        return BCrypt.hashpw(password, BCrypt.gensalt());
+//    }
+//
+//    public static boolean verify(String password, String hash){
+//        retunr BCrypt.checkpw(password, hash)
+//    }
+//}
