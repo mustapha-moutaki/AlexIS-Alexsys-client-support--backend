5c67b5d4a9dbb106fe64efa11b18ff7721ca3a63
mustapha <mustaphaamoutaki@gmail.com>
2026-04-15 12:22:45 +0100

    feat: create client class

---
+++ src/main/java/com/alexsysSolutions/alexsis/model/Client.java
@@ -0,0 +1,24 @@
+package com.alexsysSolutions.alexsis.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "clients")
+@PrimaryKeyJoinColumn(name = "id")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Client extends User {
+
+    @Column(name = "registration_date", nullable = false)
+    private LocalDateTime registrationDate;
+
+    @Column(name = "satisfaction_score")
+    private Integer satisfactionScore;
+
+}
