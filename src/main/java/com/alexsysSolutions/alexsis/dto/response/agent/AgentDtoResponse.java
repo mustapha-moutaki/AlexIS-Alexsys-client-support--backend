3ffb6eaedbc9bf0ab9a33fff687c3fd576cb46ad
mustapha <mustaphaamoutaki@gmail.com>
2026-04-16 16:18:56 +0100

    create agent dto response

---
+++ src/main/java/com/alexsysSolutions/alexsis/dto/response/agent/AgentDtoResponse.java
@@ -0,0 +1,31 @@
+package com.alexsysSolutions.alexsis.dto.response.agent;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class AgentDtoResponse {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String profilePicture;
+    private boolean active;
+    private boolean deleted;
+
+    private String specialization;
+    private String averageResolutionTime;
+    private String performanceRating;
+
+    private String availabilityStatus;
+}
