102f26c26ac473495333213a5dcec8224f27a259
mustapha <mustaphaamoutaki@gmail.com>
2026-04-16 16:19:13 +0100

    feat: create agent mapper

---
+++ src/main/java/com/alexsysSolutions/alexsis/mapper/AgentMapper.java
@@ -0,0 +1,20 @@
+package com.alexsysSolutions.alexsis.mapper;
+
+import com.alexsysSolutions.alexsis.dto.request.agent.AgentCreateDtoRequest;
+import com.alexsysSolutions.alexsis.dto.request.agent.AgentUpdateDtoRequest;
+import com.alexsysSolutions.alexsis.dto.response.agent.AgentDtoResponse;
+import com.alexsysSolutions.alexsis.model.Agent;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AgentMapper {
+    @Mapping(target = "specialization", source = "specialization")
+    @Mapping(target = "availabilityStatus", source = "availabilityStatus")
+    AgentDtoResponse toDto(Agent agent);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "password", ignore = true)
+    void updateEntity(AgentUpdateDtoRequest dto, @MappingTarget Agent agent);
+
+    Agent toEntity(AgentCreateDtoRequest dto);
+}
