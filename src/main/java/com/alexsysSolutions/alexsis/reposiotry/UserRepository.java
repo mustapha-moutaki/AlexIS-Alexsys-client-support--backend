cfb6e4723bba6ec6dab4310676db20952467a24a
mustapha <mustaphaamoutaki@gmail.com>
2026-04-16 13:10:15 +0100

    feat: fix the JPQL

--- src/main/java/com/alexsysSolutions/alexsis/reposiotry/UserRepository.java
+++ src/main/java/com/alexsysSolutions/alexsis/reposiotry/UserRepository.java
@@ -11,10 +11,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 @Repository
-public interface UserRepositroy extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 
