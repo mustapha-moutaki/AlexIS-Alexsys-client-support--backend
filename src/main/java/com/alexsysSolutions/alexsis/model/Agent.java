ce7f7358fc997aeec335c713d737fa7a844ccba4
mustapha <mustaphaamoutaki@gmail.com>
2026-04-16 11:20:56 +0100

    fix: add superBuilder to the agent clas

--- src/main/java/com/alexsysSolutions/alexsis/model/Agent.java
+++ src/main/java/com/alexsysSolutions/alexsis/model/Agent.java
@@ -2,26 +2,32 @@ package com.alexsysSolutions.alexsis.model;
 
 import com.alexsysSolutions.alexsis.enums.AvailabilityStatus;
 import com.alexsysSolutions.alexsis.enums.Specialization;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "agents")
 @PrimaryKeyJoinColumn(name = "id")
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-
 public class Agent extends User{
 
+    @Column(name = "specialization", nullable = false)
+    @Enumerated(EnumType.STRING)
     private Specialization specialization;
+
+    @Column(name = "average_resolution_time")
     private String averageResolutionTime;
+    @Column(name = "performance_rating")
     private String performanceRating;
+
+    @Column(name = "availability_status")
+    @Enumerated(EnumType.STRING)
     private AvailabilityStatus availabilityStatus;
 
 }
