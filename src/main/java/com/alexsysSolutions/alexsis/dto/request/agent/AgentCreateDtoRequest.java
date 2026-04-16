8527e3ae248ddcdb9f53ae57276fdb4031d0cf89
mustapha <mustaphaamoutaki@gmail.com>
2026-04-16 16:18:40 +0100

    feat: create agent dto reqeust

---
+++ src/main/java/com/alexsysSolutions/alexsis/dto/request/agent/AgentCreateDtoRequest.java
@@ -0,0 +1,43 @@
+package com.alexsysSolutions.alexsis.dto.request.agent;
+
+import com.alexsysSolutions.alexsis.enums.AvailabilityStatus;
+import com.alexsysSolutions.alexsis.enums.Specialization;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AgentCreateDtoRequest {
+
+    // user info
+    @NotBlank(message = "first name is required")
+    private String firstName;
+    @NotBlank(message = "last name is required")
+    private String lastName;
+    @NotBlank(message = "username is required")
+    private String username;
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is invalid")
+    private String email;
+    @NotBlank(message = "password is required")
+    @Size(min = 6, message = "the password must be at least 6 character")
+    private String password;
+    private String profilePicture;
+    private String phoneNumber;
+
+    // agent info
+    @NotNull
+    private Specialization specialization;
+    private String averageResolutionTime;
+    private String performanceRating;
+
+    private AvailabilityStatus availabilityStatus;// the default is other
+
+
+}
---
+++ src/main/java/com/alexsysSolutions/alexsis/dto/request/agent/AgentUpdateDtoRequest.java
@@ -0,0 +1,30 @@
+package com.alexsysSolutions.alexsis.dto.request.agent;
+
+import com.alexsysSolutions.alexsis.enums.AvailabilityStatus;
+import com.alexsysSolutions.alexsis.enums.Specialization;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class AgentUpdateDtoRequest {
+
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String profilePicture;
+    private boolean active;
+    private boolean deleted;
+
+    private Specialization specialization;
+    private String averageResolutionTime;
+    private String performanceRating;
+
+    private AvailabilityStatus availabilityStatus;
+}
