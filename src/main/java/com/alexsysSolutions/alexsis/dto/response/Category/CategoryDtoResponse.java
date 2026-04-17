cf7c3d717062b2abb6f0847551b04c2c680699a1
mustapha <mustaphaamoutaki@gmail.com>
2026-04-17 10:02:26 +0100

    feat: create Category dto response

---
+++ src/main/java/com/alexsysSolutions/alexsis/dto/response/Category/CategoryDtoResponse.java
@@ -0,0 +1,15 @@
+package com.alexsysSolutions.alexsis.dto.response.Category;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class CategoryDtoResponse {
+    private Long id;
+    private String name;
+    private String description;
+}
