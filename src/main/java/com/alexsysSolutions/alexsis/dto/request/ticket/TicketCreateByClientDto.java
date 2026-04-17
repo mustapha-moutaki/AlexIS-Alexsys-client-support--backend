7988bbe989c8f796262ed6fade8298fb4714767c
mustapha <mustaphaamoutaki@gmail.com>
2026-04-17 17:34:23 +0100

    feat: add validation to the ticketCreateDto

--- src/main/java/com/alexsysSolutions/alexsis/dto/request/ticket/TicketCreateByClientDto.java
+++ src/main/java/com/alexsysSolutions/alexsis/dto/request/ticket/TicketCreateByClientDto.java
@@ -2,6 +2,8 @@ package com.alexsysSolutions.alexsis.dto.request.ticket;
 
 import com.alexsysSolutions.alexsis.enums.IssueType;
 import com.alexsysSolutions.alexsis.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -11,9 +13,14 @@ import lombok.*;
 @Setter
 // Client create ticket
 public class TicketCreateByClientDto {
+    @NotBlank(message = "title is required")
     private String title;
+    @NotBlank(message = "description is required")
     private String description;
+    @NotNull(message = "priority is required")
     private Priority priority;
+    @NotNull(message = "issue type is required")
     private IssueType issueType;
+    @NotBlank(message = "category is required")
     private Long categoryId;
 }
