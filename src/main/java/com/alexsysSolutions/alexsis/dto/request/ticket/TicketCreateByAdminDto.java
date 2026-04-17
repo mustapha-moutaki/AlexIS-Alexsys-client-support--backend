23849a27f6c5c085c857e544fde5d9412e8d26dc
mustapha <mustaphaamoutaki@gmail.com>
2026-04-17 17:33:38 +0100

    faet: add validation for ticket entity

--- src/main/java/com/alexsysSolutions/alexsis/dto/request/ticket/TicketCreateByAdminDto.java
+++ src/main/java/com/alexsysSolutions/alexsis/dto/request/ticket/TicketCreateByAdminDto.java
@@ -3,6 +3,8 @@ package com.alexsysSolutions.alexsis.dto.request.ticket;
 import com.alexsysSolutions.alexsis.enums.IssueType;
 import com.alexsysSolutions.alexsis.enums.Priority;
 import com.alexsysSolutions.alexsis.enums.TicketStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -12,13 +14,23 @@ import lombok.*;
 @Setter
 // Admin Agent create ticket
 public class TicketCreateByAdminDto {
+    @NotBlank(message = "title is required")
     private String title;
-    private String description;
-    private TicketStatus status;
-    private Priority priority;
-    private IssueType issueType;
-    private Long categoryId;
 
+    private String description;
+
+    @NotNull
+    private TicketStatus status;
+
+    @NotNull
+    private Priority priority;
+
+    @NotNull
+    private IssueType issueType;
+    @NotBlank(message = "category is required")
+    private Long categoryId;
+    @NotBlank(message = "client is missing")
     private Long clientId;
+    @NotBlank(message = "assignTo filed is required")
     private Long assignedToId;
 }
