1943d5cc1ef7744712a4ee0f1b79ca6ad4627ae1
mustapha <mustaphaamoutaki@gmail.com>
2026-04-18 12:00:00 +0100

    feat: create ticket client controller-endpoint

--- src/main/java/com/alexsysSolutions/alexsis/controller/ticket/ClientTicketController.java
+++ src/main/java/com/alexsysSolutions/alexsis/controller/ticket/ClientTicketController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ClientTicketController {
 
-
     private final TicketService ticketService;
     private final TicketCommandMapper commandMapper;
 
