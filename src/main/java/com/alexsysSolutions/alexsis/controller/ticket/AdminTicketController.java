9d48822da213bbc4143478763a0fbc0e4394d00a
mustapha <mustaphaamoutaki@gmail.com>
2026-04-18 12:00:00 +0100

    feat: create ticket admin controller-endpoint

---
+++ src/main/java/com/alexsysSolutions/alexsis/controller/ticket/AdminTicketController.java
@@ -0,0 +1,43 @@
+package com.alexsysSolutions.alexsis.controller.ticket;
+
+import com.alexsysSolutions.alexsis.dto.request.ticket.TicketCreateByAdminDto;
+import com.alexsysSolutions.alexsis.dto.request.ticket.TicketCreateCommand;
+import com.alexsysSolutions.alexsis.dto.response.ApiResponse;
+import com.alexsysSolutions.alexsis.dto.response.ticket.TicketDetailDtoResponse;
+import com.alexsysSolutions.alexsis.mapper.TicketCommandMapper;
+import com.alexsysSolutions.alexsis.mapper.TicketMapper;
+import com.alexsysSolutions.alexsis.service.TicketService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin/tickets")
+public class AdminTicketController {
+    private final TicketService ticketService;
+    private final TicketMapper ticketMapper;
+    private final TicketCommandMapper ticketCommandMapper;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<TicketDetailDtoResponse>>create(
+            @Valid
+            @RequestBody TicketCreateByAdminDto dto,
+            HttpServletRequest http
+            ){
+        TicketCreateCommand command = ticketCommandMapper.fromAdminDto(dto);
+        TicketDetailDtoResponse savedTicket = ticketService.create(command);
+        ApiResponse<TicketDetailDtoResponse> response = ApiResponse.success("Ticket Created successfully", savedTicket);
+        response.setPath(http.getRequestURI());
+        response.setStatus(HttpStatus.CREATED.value());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+
+    }
+}
