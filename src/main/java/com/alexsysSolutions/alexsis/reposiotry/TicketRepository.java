5001ca064d3eca52702b2e851662516188b522ca
mustapha <mustaphaamoutaki@gmail.com>
2026-04-18 12:00:00 +0100

    feat: create ticket repository with query to avoid n+1

---
+++ src/main/java/com/alexsysSolutions/alexsis/reposiotry/TicketRepository.java
@@ -0,0 +1,26 @@
+package com.alexsysSolutions.alexsis.reposiotry;
+
+import com.alexsysSolutions.alexsis.model.Ticket;
+import org.apache.commons.text.translate.NumericEntityUnescaper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    // to avoid N+1 issue
+    @Query("""
+    SELECT t FROM Ticket t
+    LEFT JOIN FETCH t.comments
+    LEFT JOIN FETCH t.attachments
+    LEFT JOIN FETCH t.category
+    LEFT JOIN FETCH t.client
+    LEFT JOIN FETCH t.assignedTo
+    WHERE t.id = :id
+    """)
+    Optional<Ticket> findByIdWithDetails(Long id);
+
+    boolean existsById(Long id);
+}
