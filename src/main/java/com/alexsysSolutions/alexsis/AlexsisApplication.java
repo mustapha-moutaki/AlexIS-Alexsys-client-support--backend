83bd5707cd07659e5ce5dd97a6ebc617df711aa1
mustapha <mustaphaamoutaki@gmail.com>
2026-04-19 12:00:00 +0100

    fix: add @Annotation to enable reading JwtProperties

--- src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
+++ src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
@@ -2,7 +2,9 @@ package com.alexsysSolutions.alexsis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan// enable ConfigurationProperties
 @SpringBootApplication
 public class AlexsisApplication {
 
