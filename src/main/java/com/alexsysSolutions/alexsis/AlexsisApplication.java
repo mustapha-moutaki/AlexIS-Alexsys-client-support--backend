93772800a56e4c7ce1b5616ede0699be4cd086ed
mustapha <mustaphaamoutaki@gmail.com>
2026-04-22 13:35:42 +0100

    feat: add anotation in the main method to enable the sheduledCrono

--- src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
+++ src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
@@ -3,8 +3,10 @@ package com.alexsysSolutions.alexsis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan// enable ConfigurationProperties
+@EnableScheduling // to enable @Sheduled in the service
 @SpringBootApplication
 public class AlexsisApplication {
 
