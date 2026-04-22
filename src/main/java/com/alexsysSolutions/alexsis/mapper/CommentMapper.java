deeef554966ca5691ab79f7d46dca9a62e36ab39
mustapha <mustaphaamoutaki@gmail.com>
2026-04-22 10:37:08 +0100

    fix: ignore the author attribute to linked it in the service

--- src/main/java/com/alexsysSolutions/alexsis/mapper/CommentMapper.java
+++ src/main/java/com/alexsysSolutions/alexsis/mapper/CommentMapper.java
@@ -5,6 +5,7 @@ import com.alexsysSolutions.alexsis.dto.response.comment.CommentDtoResponse;
 import com.alexsysSolutions.alexsis.model.Comment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
@@ -13,7 +14,10 @@ public interface CommentMapper {
     @Mapping(source = "author.username", target = "authorName")
     @Mapping(source = "createdAt", target = "createdAt")
     CommentDtoResponse toDto(Comment comment);
-
+    @Mappings({
+            @Mapping(target ="ticket", ignore = true),
+            @Mapping(target ="author", ignore = true)
+    })
     Comment toEntity(CommentDtoRequest dto);
 
 }
