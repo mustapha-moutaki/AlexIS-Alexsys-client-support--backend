4d35c5914ed091839f775aeacce0528ab0e4f52c
mustapha <mustaphaamoutaki@gmail.com>
2026-04-23 13:28:09 +0100

    fix: add delay minutes for each priority

--- src/main/java/com/alexsysSolutions/alexsis/enums/Priority.java
+++ src/main/java/com/alexsysSolutions/alexsis/enums/Priority.java
@@ -1,8 +1,18 @@
 package com.alexsysSolutions.alexsis.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH,
-    CRITICAL
+    CRITICAL(7), // 7 min
+    HIGH(60), // 1 hour
+    MEDIUM(240), // 4 hours
+    LOW(1440); // 1 day
+
+    private final int delayInMinutes;
+
+    Priority(int delayInMinutes) {
+        this.delayInMinutes = delayInMinutes;
+    }
+
 }
