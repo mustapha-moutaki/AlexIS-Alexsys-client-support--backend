dbc4f0aae67be50442a07f037156cf6205e6c835
mustapha <mustaphaamoutaki@gmail.com>
2026-04-24 09:58:44 +0100

    feat: create dashaboardService

---
+++ src/main/java/com/alexsysSolutions/alexsis/service/DashboardService.java
@@ -0,0 +1,7 @@
+package com.alexsysSolutions.alexsis.service;
+
+import com.alexsysSolutions.alexsis.dto.response.dashboard.DashboardOverViewDto;
+
+public interface DashboardService {
+    DashboardOverViewDto getOverview();
+}
