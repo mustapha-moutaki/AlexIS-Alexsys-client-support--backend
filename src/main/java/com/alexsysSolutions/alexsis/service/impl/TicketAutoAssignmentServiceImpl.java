ca649d7708ebdbd072af701f5ac31b6c59718bec
mustapha <mustaphaamoutaki@gmail.com>
2026-04-24 09:57:09 +0100

    feat: ticket autoAssignment after checking while based on the priority and agent availables

--- src/main/java/com/alexsysSolutions/alexsis/service/impl/TicketAutoAssignmentServiceImpl.java
+++ src/main/java/com/alexsysSolutions/alexsis/service/impl/TicketAutoAssignmentServiceImpl.java
@@ -6,21 +6,26 @@ import com.alexsysSolutions.alexsis.model.Agent;
 import com.alexsysSolutions.alexsis.model.User;
 import com.alexsysSolutions.alexsis.reposiotry.AgentRepository;
 import com.alexsysSolutions.alexsis.service.TicketAutoAssignmentService;
+import com.alexsysSolutions.alexsis.service.WorkloadService;
 import com.alexsysSolutions.alexsis.service.impl.assignment.factory.AssignmentStrategyFactory;
 import com.alexsysSolutions.alexsis.service.strategy.AssignmentStrategy;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class TicketAutoAssignmentServiceImpl implements TicketAutoAssignmentService {
     private final AgentRepository agentRepository;
     private final AssignmentStrategyFactory strategyFactory;
+    private final WorkloadService workloadService;
 
     @Override
-    public User assignAgent(Priority priority) {
+    public Agent assignAgent(Priority priority) {
 
         List<Agent> agents = agentRepository.findAvailableAgents();
 
@@ -30,6 +35,12 @@ public class TicketAutoAssignmentServiceImpl implements TicketAutoAssignmentServ
 
         AssignmentStrategy strategy = strategyFactory.getStrategy(priority);
 
-        return strategy.assign(agents);
+        Agent agent = strategy.assign(agents);
+
+        workloadService.incrementTicketsActiveCount(agent);
+
+        agentRepository.save(agent);
+
+        return agent;
     }
 }
