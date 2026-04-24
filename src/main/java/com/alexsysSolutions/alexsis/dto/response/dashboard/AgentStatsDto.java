eb93198d3d50f95674b5cef1e877a417e4304aa1
mustapha <mustaphaamoutaki@gmail.com>
2026-04-24 10:31:40 +0100

    feat: complete agentDtoStats

--- src/main/java/com/alexsysSolutions/alexsis/dto/response/dashboard/AgentStatsDto.java
+++ src/main/java/com/alexsysSolutions/alexsis/dto/response/dashboard/AgentStatsDto.java
@@ -6,14 +6,16 @@ import lombok.Data;
 @Data
 public class AgentStatsDto {
 
-    private int totalAvailableAgents;
+    private int totalAgents; // total number of agents
     private int totalBusyAgents;
+    private int totalAvailableAgents;
     private int overloadAgents; // activeTicketCount > maxCapacity
-    private Double avgPerformanceRating;
+    private Double averageResolutionTime;
+    private Double averagePerformanceRating;
     private String bestAgent; // "Agent Name (ID: 123)" based on max rating
     private double avgLoadPerAgent; // average active tickets per agent
     private User riskDetected; // agent overloaded with low performance ( based on score: performance_rating - (activeTicketCount - maxCapacity))
-    private int totalAgents; // total number of agents
+
 
 }
 
