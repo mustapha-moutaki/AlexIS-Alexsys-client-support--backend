2291a72ebb0d2eac70e78b9515f75b27102b494d
mustapha <mustaphaamoutaki@gmail.com>
2026-04-24 14:18:58 +0100

    feat: create and fix queries in the clientRepository

--- src/main/java/com/alexsysSolutions/alexsis/reposiotry/ClientRepository.java
+++ src/main/java/com/alexsysSolutions/alexsis/reposiotry/ClientRepository.java
@@ -1,28 +1,53 @@
 package com.alexsysSolutions.alexsis.reposiotry;
 
 import com.alexsysSolutions.alexsis.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    Optional<Client> findByEmail(String email);
+    Optional<Client>findByUsername(String username);
+    Optional<Client> findById(Long id);
+
+    @Query("""
+SELECT c FROM Client c
+WHERE (:isVip IS NULL OR c.isVip = :isVip)
+  AND (:isActive IS NULL OR c.active = :isActive)
+""")
+    Page<Client> findAllWithFilters(
+            @Param("isVip") Boolean isVip,
+            @Param("isActive") Boolean isActive,
+            Pageable pageable
+    );
 
     // stats queries
-    @Query("SELECT count(*) FROM Client c")
-    int countTotalClients();
+    @Query("SELECT COUNT(c) FROM Client c")
+    long countTotalClients();
 
-   @Query("SELECT count(*) FROM Client c where c.active=true AND c.deleted=false")
-    int countActiveClients();
+    @Query("SELECT COUNT(c) FROM Client c WHERE c.active = true AND c.deleted = false")
+    long countActiveClients();
 
-   @Query("SELECT count(*) FROM Client c WHERE DATE(c.registrationDate) = CURRENT_DATE")
-    int countClientsRegisteredToday();
+    @Query("SELECT COUNT(c) FROM Client c WHERE FUNCTION('DATE', c.registrationDate) = CURRENT_DATE")
+    long countClientsRegisteredToday();
 
-    @Query("SELECT avg(c.satisfactionScore) FROM Client c")
+    @Query("SELECT AVG(c.satisfactionScore) FROM Client c")
     Double averageSatisfactionScore();
 
-    @Query("SELECT count(*) FROM Client c WHERE c.satisfactionScore < 3")
-    int countLowSatisfactionClients();
+    @Query("SELECT COUNT(c) FROM Client c WHERE c.satisfactionScore < 3")
+    long countLowSatisfactionClients();
+
+//     @Query("SELECT c.firstName, c.lastName FROM Client c " +
+//             "WHERE c.satisfactionScore = (SELECT MIN(satisfactionScore) FROM Client)")
+//    String findMostDissatisfiedClient();
+
 
 }
