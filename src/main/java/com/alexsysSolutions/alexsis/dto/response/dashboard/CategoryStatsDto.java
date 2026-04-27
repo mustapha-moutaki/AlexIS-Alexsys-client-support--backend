729e10c82cbe6032a4ebb37562413ecfa3b89c74
mustapha <mustaphaamoutaki@gmail.com>
2026-04-27 11:28:01 +0100

    fix: return category endpoint just categories stats

---
+++ src/main/java/com/alexsysSolutions/alexsis/dto/response/dashboard/CategoryStatsDto.java
@@ -0,0 +1,12 @@
+package com.alexsysSolutions.alexsis.dto.response.dashboard;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryStatsDto {
+    private int totalCategories;
+}
