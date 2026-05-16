deb2b95675a58e2e849484bc86ba6fa682a0bf1c
mustapha <mustaphaamoutaki@gmail.com>
2026-05-16 14:47:41 +0100

    feat: use query to get all comments for single ticket

--- src/main/java/com/alexsysSolutions/alexsis/reposiotry/CommentRepository.java
+++ src/main/java/com/alexsysSolutions/alexsis/reposiotry/CommentRepository.java
@@ -4,10 +4,21 @@ import com.alexsysSolutions.alexsis.model.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // st: find all comments by ticket id
     Page<Comment> findByTicketId(Long ticketId, Pageable pageable);
+
+    @Query("""
+    SELECT c
+    FROM Comment c
+    JOIN FETCH c.author
+    WHERE c.id = :id
+""")
+    Optional<Comment> findByIdWithAuthor(Long id);
 }
