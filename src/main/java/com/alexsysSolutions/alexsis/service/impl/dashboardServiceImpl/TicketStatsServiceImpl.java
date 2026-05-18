1a66cd241d4ce4df75a1aee5db913b64b96d6d97
mustapha <mustaphaamoutaki@gmail.com>
2026-05-18 09:44:15 +0100

    feat: create method in service to retunr the data of graphs

--- src/main/java/com/alexsysSolutions/alexsis/service/impl/dashboardServiceImpl/TicketStatsServiceImpl.java
+++ src/main/java/com/alexsysSolutions/alexsis/service/impl/dashboardServiceImpl/TicketStatsServiceImpl.java
@@ -1,9 +1,13 @@
 package com.alexsysSolutions.alexsis.service.impl.dashboardServiceImpl;
 
+import com.alexsysSolutions.alexsis.dto.response.dashboard.graphs.ResolutionTimeTrendStatsDtoResponse;
+import com.alexsysSolutions.alexsis.dto.response.dashboard.graphs.WeeklyTicketsStatsDtoResponse;
 import com.alexsysSolutions.alexsis.reposiotry.TicketRepository;
 import com.alexsysSolutions.alexsis.service.dashboardService.TicketStatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -44,5 +48,27 @@ public class TicketStatsServiceImpl implements TicketStatsService {
     @Override
     public int totalTicketsToday() {
         return ticketRepository.totalTicketsToday();
+    }
+
+    @Override
+    public List<ResolutionTimeTrendStatsDtoResponse> getResolutionTimeTrendStats() {
+        return ticketRepository.getResolutionTimeTrend().stream()
+                .map(row -> new ResolutionTimeTrendStatsDtoResponse(
+                        (String) row[0],
+                        ((Number) row[1]).longValue()
+                ))
+                .toList();
+    }
+
+    @Override
+    public List<WeeklyTicketsStatsDtoResponse> getWeeklyTicketsStats() {
+         return ticketRepository.getWeeklyTicketsStats()
+                .stream()
+                .map(row -> new WeeklyTicketsStatsDtoResponse(
+                        (String) row[0],
+                        ((Number) row[1]).longValue(),
+                        ((Number) row[2]).longValue()
+                ))
+                .toList();
     }
 }
