e0654ae355f43b56b95d8e5ec31bb37a8c67cd8d
mustapha <mustaphaamoutaki@gmail.com>
2026-05-18 09:43:39 +0100

    feat: create method in service to retunr the data of graphs

--- src/main/java/com/alexsysSolutions/alexsis/service/dashboardService/ClientStatsService.java
+++ src/main/java/com/alexsysSolutions/alexsis/service/dashboardService/ClientStatsService.java
@@ -1,5 +1,9 @@
 package com.alexsysSolutions.alexsis.service.dashboardService;
 
+import com.alexsysSolutions.alexsis.dto.response.dashboard.graphs.SatisfactionTrendStatsDtoResponse;
+
+import java.util.List;
+
 public interface ClientStatsService {
      int totalClients();
      int totalActiveClients(); // active clients (not deleted)
@@ -7,4 +11,6 @@ public interface ClientStatsService {
      Double avgSatisfactionScore(); // average satisfaction score across clients
      int lowSatisfactionClients();
 
+     // for graphs
+     List<SatisfactionTrendStatsDtoResponse> getSatisfactionTrendStats();
 }
