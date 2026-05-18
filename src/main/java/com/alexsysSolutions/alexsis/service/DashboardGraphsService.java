866635135760bbdff9389473e23fac80e9d8b075
mustapha <mustaphaamoutaki@gmail.com>
2026-05-18 09:45:05 +0100

    feat: create dashaboard graphs service

---
+++ src/main/java/com/alexsysSolutions/alexsis/service/DashboardGraphsService.java
@@ -0,0 +1,7 @@
+package com.alexsysSolutions.alexsis.service;
+
+import com.alexsysSolutions.alexsis.dto.response.dashboard.graphs.DashboardGraphsDtoResponse;
+
+public interface DashboardGraphsService {
+    DashboardGraphsDtoResponse getDashboardGraphsData();
+}
