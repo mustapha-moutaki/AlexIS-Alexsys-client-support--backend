f858abbb04bb459f777ae56407e1c214b5310667
mustapha <mustaphaamoutaki@gmail.com>
2026-05-20 15:03:09 +0100

    feat: add new endpoint -change the status to resolved -getTicketByIdAssinged to authenticated agent

--- src/main/java/com/alexsysSolutions/alexsis/controller/ticket/AgentTicketController.java
+++ src/main/java/com/alexsysSolutions/alexsis/controller/ticket/AgentTicketController.java
@@ -1,5 +1,6 @@
 package com.alexsysSolutions.alexsis.controller.ticket;
 
+import com.alexsysSolutions.alexsis.dto.request.ticket.TicketUpdateStatusDtoRequest;
 import com.alexsysSolutions.alexsis.dto.response.ApiResponse;
 import com.alexsysSolutions.alexsis.dto.response.ticket.TicketDetailDtoResponse;
 import com.alexsysSolutions.alexsis.dto.response.ticket.TicketSummaryDtoResponse;
@@ -9,15 +10,13 @@ import com.alexsysSolutions.alexsis.service.TicketService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -76,6 +75,49 @@ public class AgentTicketController {
         response.setStatus(HttpStatus.OK.value());
         response.setPath(http.getRequestURI());
 
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(
+            summary = "Get ticket by ID for agent",
+            description = "Retrieve a single ticket assigned to the authenticated agent"
+    )
+    public ResponseEntity<ApiResponse<TicketDetailDtoResponse>>getTicketByIdForAgent(
+            @PathVariable Long id,
+           HttpServletRequest http
+    ){
+        logger.info("getting ticket by id {} that assigned to agent with id {}", id, currentUserProvider.getUserId());
+        TicketDetailDtoResponse ticket = ticketService.getTicketByIdForAgent(id);
+
+        ApiResponse<TicketDetailDtoResponse>response = ApiResponse.success("Ticket received successfully", ticket);
+        response.setStatus(HttpStatus.OK.value());
+        response.setPath(http.getRequestURI());
+        logger.info("ticket received successfully");
+        logger.warn(response.toString());
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(
+            summary = "Update ticket status",
+            description = "Update the status of a ticket assigned to the authenticated agent"
+    )
+    public ResponseEntity<ApiResponse<TicketSummaryDtoResponse>>updateTicketStatus(
+            @Valid
+            @PathVariable Long id,
+          @RequestBody TicketUpdateStatusDtoRequest dto,
+          HttpServletRequest http
+          ){
+        logger.info("PUT /api/v1/agent/tickets/{} - Updating ticket status | agentId: {} | newStatus: {}",
+                id,
+                currentUserProvider.getUserId(),
+                dto.getStatus()
+        );
+        TicketSummaryDtoResponse updatedTicketStatus = ticketService.updateTicketStatus(id, dto);
+        ApiResponse<TicketSummaryDtoResponse> response = ApiResponse.success("Ticket status updated successfully", updatedTicketStatus);
+        response.setPath(http.getRequestURI());
+        response.setStatus(HttpStatus.OK.value());
         return ResponseEntity.ok(response);
     }
 }
