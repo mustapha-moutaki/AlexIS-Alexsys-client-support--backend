2f31b4accfaeac8d1a6490c142dbce86f607c5ac
mustapha <mustaphaamoutaki@gmail.com>
2026-05-21 14:53:23 +0100

    fix: creating attachment to stoe the admin d

--- src/main/java/com/alexsysSolutions/alexsis/dto/request/attachment/AttachmentCreateDtoRequest.java
+++ src/main/java/com/alexsysSolutions/alexsis/dto/request/attachment/AttachmentCreateDtoRequest.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class AttachmentCreateDtoRequest {
 
     MultipartFile file;
-    String uploadedBy;
     private Long ticketId;
+    private Long uploadedById;
+    private String uploadedByName;
 }
