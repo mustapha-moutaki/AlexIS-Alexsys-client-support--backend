b139d95d880372bfbaa3ccf304bb12a492e9848e
mustapha <mustaphaamoutaki@gmail.com>
2026-06-09 15:01:51 +0100

    feat: update OpenAI provider integration and configurations

--- src/main/java/com/alexsysSolutions/alexsis/client/OpenAiProvider.java
+++ src/main/java/com/alexsysSolutions/alexsis/client/OpenAiProvider.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -20,7 +21,8 @@ public class OpenAiProvider implements AiProvider {
 
     private final Logger logger = LoggerFactory.getLogger(OpenAiProvider.class);
 
-    public OpenAiProvider(WebClient webClient,
+    public OpenAiProvider(
+                          @Qualifier("openAiWebClient") WebClient webClient,
                           OpenAiProperties properties,
                           ObjectMapper objectMapper) {
         this.webClient = webClient;
