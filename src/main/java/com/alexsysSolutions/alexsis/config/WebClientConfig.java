64629d2aec07f2ae11218aabc1e503ceeb23c860
mustapha <mustaphaamoutaki@gmail.com>
2026-06-09 15:01:52 +0100

    feat: add WebClient configuration for reactive API calls

---
+++ src/main/java/com/alexsysSolutions/alexsis/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.alexsysSolutions.alexsis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient emailWebClient(WebClient.Builder builder) {
+
+        return builder
+                .baseUrl("http://172.17.0.1:8081")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+}
+// we use webClient  to consume email server
