afb630944800006cdb0a72209cf3fad2703ea7ac
mustapha <mustaphaamoutaki@gmail.com>
2026-06-09 15:01:52 +0100

    feat: update main application entry point and configurations

--- src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
+++ src/main/java/com/alexsysSolutions/alexsis/AlexsisApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableConfigurationProperties(OpenAiProperties.class) // to handle the properties configuration
 @ConfigurationPropertiesScan// enable ConfigurationProperties
 @EnableScheduling // to enable @Sheduled in the service
 @SpringBootApplication
+@EnableAsync // enable async in the applicaiton
 public class AlexsisApplication {
 
 	public static void main(String[] args) {
