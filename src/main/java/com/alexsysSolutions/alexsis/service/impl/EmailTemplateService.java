0c5c3730103af9ac6bb12661c77a7bd3a98132ca
mustapha <mustaphaamoutaki@gmail.com>
2026-06-09 15:01:51 +0100

    feat: implement logic for managing email templates

--- src/main/java/com/alexsysSolutions/alexsis/service/impl/EmailTemplateService.java
+++ src/main/java/com/alexsysSolutions/alexsis/service/impl/EmailTemplateService.java
@@ -1,4 +1,31 @@
 package com.alexsysSolutions.alexsis.service.impl;
 
-public class EmailTemplateService {
+import com.alexsysSolutions.alexsis.enums.UserRole;
+import com.alexsysSolutions.alexsis.service.IEmailTemplateService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailTemplateService implements IEmailTemplateService {
+    public EmailTemplateService(){
+
+    }
+
+    @Override
+    public String buildWelcomeEmailTemplate(String username) {
+        return "Dear " + username + ",\n\n" +
+                "Welcome to our service! We're excited to have you on board.\n\n" +
+                "Best regards,\n" +
+                "AlexIS Team";
+    }
+
+    @Override
+    public String buildAdminNotificationTemplate(String username, String email, UserRole role) {
+        return "Admin Notification:\n\n" +
+                "You just created new user " + username + " successfully.\n\n" +
+                "Email " + email + ".\n\n" +
+                "Role " + role + ".\n\n" +
+                "Best regards,\n" +
+                "The System";
+    }
 }
