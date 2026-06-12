c28759f473048afedec42719e56e61c4ab564a86
mustapha <mustaphaamoutaki@gmail.com>
2026-06-12 05:00:00 +0100

    use groq ai

--- src/main/java/com/alexsysSolutions/alexsis/service/impl/GroqService.java
+++ src/main/java/com/alexsysSolutions/alexsis/service/impl/GroqService.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
