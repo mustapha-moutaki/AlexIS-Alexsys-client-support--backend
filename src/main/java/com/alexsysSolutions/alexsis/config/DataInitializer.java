1c4e64b1b8d7c5829d55cd755e5139552bddb0c4
mustapha <mustaphaamoutaki@gmail.com>
2026-06-12 16:45:00 +0100

    feat: update data initialization

--- src/main/java/com/alexsysSolutions/alexsis/config/DataInitializer.java
+++ src/main/java/com/alexsysSolutions/alexsis/config/DataInitializer.java
@@ -5,6 +5,8 @@ import com.alexsysSolutions.alexsis.model.User;
 import com.alexsysSolutions.alexsis.reposiotry.UserRepository;
 import com.alexsysSolutions.alexsis.util.PasswordUtil;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,11 +19,12 @@ import java.time.LocalDateTime;
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
 
     @Override
     public void run(String... args) throws Exception {
         boolean exists = userRepository.existsByRole(UserRole.SUPER_ADMIN);
-
+        boolean existsAdminByEmail = userRepository.existsByEmail("jhonDoe@gmail.com");
         if(!exists){
             User superAdmin = User.builder()
                     .username("superadmin")
@@ -35,6 +38,22 @@ public class DataInitializer implements CommandLineRunner {
                     .build();
 
             userRepository.save(superAdmin);
+            logger.info("Super admin user created with email: {}", superAdmin.getEmail());
         }
+
+        if (!existsAdminByEmail) {
+            User adminUser = User.builder()
+                    .username("jhondoe")
+                    .firstName("John")
+                    .lastName("Doe")
+                    .email("jhonDoe@gmail.com")
+                    .password(PasswordUtil.hash("password")) // Hashing the password as per your utility
+                    .role(UserRole.ADMIN)
+                    .updatedAt(LocalDateTime.now())
+                    .createdBy("system-auto-run")
+                    .build();
+
+            userRepository.save(adminUser);
+            logger.info("Admin user created with email: {}", adminUser.getEmail());
     }
-}
+}}
