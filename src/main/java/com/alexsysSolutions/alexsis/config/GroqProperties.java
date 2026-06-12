53500dc790c4a004284618bf5c06eed663de313c
mustapha <mustaphaamoutaki@gmail.com>
2026-06-12 05:00:00 +0100

    use groq ai

--- src/main/java/com/alexsysSolutions/alexsis/config/GroqProperties.java
+++ src/main/java/com/alexsysSolutions/alexsis/config/GroqProperties.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Setter
 public class GroqProperties {
 
-
     private String apiKey;
     private String url;
     private String model;
